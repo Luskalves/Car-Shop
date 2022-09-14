3d4252b76e28536ee8cf3e11e10f2fa1bd48448d
Luskalves <lusca.alves32@gmail.com>
2022-09-14 18:19:58 -0300

    adcionando rotas e validador de corpo no app

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,13 @@
 import express from 'express';
+import errorMiddleware from './errors/errorMiddleware';
+import carsRoute from './routes/carRoute';
 
 const app = express();
+
+app.use(express.json());
+
+app.use('/cars', carsRoute);
+
+app.use(errorMiddleware);
 
 export default app;
