84989c182e86c116193ac583efbf65ad54fb3959
Luskalves <lusca.alves32@gmail.com>
2022-09-14 19:15:43 -0300

    Removendo comentários desnecessários

--- src/tests/unit/models/Models.test.ts
+++ src/tests/unit/models/Models.test.ts
@@ -1,26 +1,3 @@
-// template para criação dos testes de cobertura da camada de model
-
-
-// import * as sinon from 'sinon';
-// import chai from 'chai';
-// const { expect } = chai;
-
-// describe('Sua descrição', () => {
-
-//   before(async () => {
-//     sinon
-//       .stub()
-//       .resolves();
-//   });
-
-//   after(()=>{
-//     sinon.restore();
-//   })
-
-//   it('', async () => {});
-
-// });
-
 import { expect } from 'chai';
 import sinon from 'sinon';
 import CarModel from '../../../models/CarModel';
