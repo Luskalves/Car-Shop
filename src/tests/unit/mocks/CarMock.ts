f16e306cfe9b337b24111f21c10e28cf5d045100
Luskalves <lusca.alves32@gmail.com>
2022-09-14 15:46:02 -0300

    carMock criado

---
+++ src/tests/unit/mocks/CarMock.ts
@@ -0,0 +1,10 @@
+import { ICar } from '../../../interfaces/ICar';
+
+export const CarMock: ICar = {
+  doorsQty: 2,
+  seatsQty: 5,
+  model: 'fusca azul',
+  year: 1954,
+  color: 'AzulZão',
+  buyValue: 999999999
+}
