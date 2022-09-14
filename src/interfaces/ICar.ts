3cc27454843b04fafc35ed4c39e518096c942655
Luskalves <lusca.alves32@gmail.com>
2022-09-14 18:27:42 -0300

    status no zod é opcional agora

--- src/interfaces/ICar.ts
+++ src/interfaces/ICar.ts
@@ -12,6 +12,7 @@ const carZodSchema = z.object({
   model: z.string().min(3),
   year: z.number().min(1900).max(2022),
   color: z.string().min(3),
+  status: z.boolean().optional(),
   buyValue: z.number(), 
 });
 
