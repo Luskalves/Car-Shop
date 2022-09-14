d601b3dad4c109576ccca4028ca7a27e03954beb
Luskalves <lusca.alves32@gmail.com>
2022-09-14 18:21:48 -0300

    Zod adcionando na interface ICar

--- src/interfaces/ICar.ts
+++ src/interfaces/ICar.ts
@@ -1,6 +1,18 @@
+import { z } from 'zod';
 import { IVehicle } from './IVehicle';
 
 export interface ICar extends IVehicle {
   doorsQty: number,
   seatsQty: number,
 }
+
+const carZodSchema = z.object({
+  doorsQty: z.number().min(2).max(4),
+  seatsQty: z.number().min(2).max(7),
+  model: z.string().min(3),
+  year: z.number().min(1900).max(2022),
+  color: z.string().min(3),
+  buyValue: z.number(), 
+});
+
+export { carZodSchema };
