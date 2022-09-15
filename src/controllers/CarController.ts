9dc3fe52dc995f65f445c28922ec000486870523
Luskalves <lusca.alves32@gmail.com>
2022-09-15 16:04:16 -0300

    adcionando metodo read no controller

--- src/controllers/CarController.ts
+++ src/controllers/CarController.ts
@@ -5,12 +5,27 @@ import { ICar } from '../interfaces/ICar';
 class CarController {
   constructor(private _carService: IService<ICar>) {}
 
+  public async read(_req: Request, res: Response) {
+    const cars = await this._carService.read();
+    
+    return res.status(200).json(cars);
+  }
+
   public async create(req: Request, res: Response<ICar>) {
     const car = req.body;
 
     const newCar = await this._carService.create(car);
 
     res.status(201).json(newCar);
+  }
+
+  public async update(req: Request, res: Response<ICar>) {
+    const { id } = req.params;
+    const obj = req.body;
+
+    const updatedCar = await this._carService.updateOne(id, obj);
+
+    return res.status(200).json(updatedCar);
   }
 }
 
