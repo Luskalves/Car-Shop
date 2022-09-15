fe88aaf330b751e367a6bb3769262006dc9672ef
Luskalves <lusca.alves32@gmail.com>
2022-09-15 16:03:57 -0300

    mudando erro de ortografia

--- src/errors/errorMiddleware.ts
+++ src/errors/errorMiddleware.ts
@@ -3,7 +3,7 @@ import { ErrorRequestHandler } from 'express';
 const errorMiddleware: ErrorRequestHandler = (err, _req, res, _next) => {
   const { status, message } = err;
 
-  if (!status) return res.status(500).json({ error: 'something got wrong!' });
+  if (!status) return res.status(500).json({ error: 'something went wrong!' });
 
   return res.status(status).json({ error: message });
 };
