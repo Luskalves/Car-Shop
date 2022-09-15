6f6e4a19d9c5b1e8e2932629806f29f5b2e2a5a4
Luskalves <lusca.alves32@gmail.com>
2022-09-15 16:21:10 -0300

    readOne adcionado no IService

--- src/interfaces/IService.ts
+++ src/interfaces/IService.ts
@@ -1,5 +1,6 @@
 export interface IService<T> {
   read(): Promise<T[] | []>,
   create(obj: unknown): Promise<T>,
+  readOne(id: string): Promise<T>,
   updateOne(id: string, obj: unknown): Promise<T>,
 }
