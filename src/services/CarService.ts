4a438f8a5305c989c1c7458ccfa12aab5eca441f
Luskalves <lusca.alves32@gmail.com>
2022-09-15 16:05:27 -0300

    adcionando o metodo Read no carService

--- src/services/CarService.ts
+++ src/services/CarService.ts
@@ -2,9 +2,18 @@ import { IService } from '../interfaces/IService';
 import { ICar, carZodSchema } from '../interfaces/ICar';
 import { IModel } from '../interfaces/IModel';
 import BadRequest from '../errors/customErrors/BadRequest';
+import NotFound from '../errors/customErrors/NotFound';
 
 class CarService implements IService<ICar> {
   constructor(private carModel: IModel<ICar>) {}
+
+  public async read(): Promise<ICar[] | []> {
+    const cars = await this.carModel.read();
+
+    if (!cars) return [];
+
+    return cars as ICar[];
+  }
 
   public async create(obj: ICar): Promise<ICar> {
     const parsed = carZodSchema.safeParse(obj);
@@ -13,6 +22,19 @@ class CarService implements IService<ICar> {
     const newCar = await this.carModel.create(parsed.data);
 
     return newCar;
+  }
+
+  public async updateOne(id: string, obj: unknown): Promise<ICar> {
+    if (id.length !== 24) throw new BadRequest('Id must have 24 hexadecimal characters');
+
+    const parsed = carZodSchema.safeParse(obj);
+    if (!parsed.success) throw new BadRequest('Bad Request');
+
+    const updatedCar = await this.carModel.update(id, parsed.data);
+
+    if (!updatedCar) throw new NotFound('Object not found');
+
+    return updatedCar;
   }
 }
 
