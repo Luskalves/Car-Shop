9c1096fc7f122b26eedb75f650f28926ee06f705
Luskalves <lusca.alves32@gmail.com>
2022-09-15 16:05:38 -0300

    requisito 8 concluido

--- src/models/MongoModel.ts
+++ src/models/MongoModel.ts
@@ -1,4 +1,5 @@
-import { Model } from 'mongoose';
+import { isValidObjectId, Model, UpdateQuery } from 'mongoose';
+import NotFound from '../errors/customErrors/NotFound';
 
 import { IModel } from '../interfaces/IModel';
 
@@ -12,18 +13,29 @@ abstract class MongoModel<T> implements IModel<T> {
   async create(obj: T): Promise<T> {
     return this._model.create({ ...obj });
   }
+
   async read(): Promise<T[]> {
     const response = await this._model.find();
     return response;
   }
+
   async readOne(_id: string): Promise<T | null> {
     if (!_id) return null;
     return this._model.findById(_id);
   }
+
   async update(_id: string, obj: T): Promise<T | null> {
-    if (!_id || !obj) return null;
-    return this._model.findById({ id: _id });
+    if (!isValidObjectId(_id)) throw new NotFound('Object not found');
+
+    const updatedCar = await this._model.findByIdAndUpdate(
+      { _id },
+      { ...obj } as UpdateQuery<T>,
+      { new: true },  
+    );
+
+    return updatedCar as unknown as T;
   }
+
   async delete(_id: string): Promise<T | null> {
     if (!_id) return null;
     return this._model.findById(_id);
