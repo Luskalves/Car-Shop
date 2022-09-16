b4b8c7d76c712f7f42507a185e327a0b194133fc
Luskalves <lusca.alves32@gmail.com>
2022-09-16 19:32:35 -0300

    validador de schema com zod criado no IMotorcycle

--- src/interfaces/IMotorcycle.ts
+++ src/interfaces/IMotorcycle.ts
@@ -1,6 +1,19 @@
+import { z } from 'zod';
 import { IVehicle } from './IVehicle';
 
+type MotorCategory = 'Street' | 'Custom' | 'Trail';
+
 export interface IMotorcycle extends IVehicle {
-  category: 'Street' | 'Custom' | 'Trail';
+  category: MotorCategory;
   engineCapacity: number;
 }
+
+export const motorZodSchema = z.object({
+  buyValue: z.number(),
+  category: z.enum(['Street', 'Custom', 'Trail']),
+  color: z.string(),
+  engineCapacity: z.number().min(1).max(2500),
+  model: z.string(),
+  status: z.boolean().optional(),
+  year: z.number().min(1900).max(2022),
+});
