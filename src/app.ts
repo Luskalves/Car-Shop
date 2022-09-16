e8c6a696e5370245f76a6f413399c76485b2e813
Luskalves <lusca.alves32@gmail.com>
2022-09-16 19:32:07 -0300

    rota de motorcycles adcionada no app

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,14 @@
 import express from 'express';
 import errorMiddleware from './errors/errorMiddleware';
 import carsRoute from './routes/carRoute';
+import motorRoute from './routes/motorRoute';
 
 const app = express();
 
 app.use(express.json());
 
 app.use('/cars', carsRoute);
+app.use('/motorcycles', motorRoute);
 
 app.use(errorMiddleware);
 
